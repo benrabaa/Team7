09bac47c56ac10d6bed9a307c2704e839afb1d60
rzmorales <robertzaratemorales@pursuit.org>
2018-12-04 23:18:18 -0500

    merged conflict

--- app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/MainActivity.java
+++ app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/MainActivity.java
@@ -12,14 +12,11 @@ import android.widget.Toolbar;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MenuItem;
+
 
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
+    public DrawerLayout drawerLayout;
 
 
     @Override
@@ -35,7 +32,44 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_button);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
+         drawerLayout = findViewById(R.id.drawer_layout);
+
+        final NavigationView navigationView1 = findViewById(R.id.nav_view);
+
+        navigationView1.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_Pilin:
+                        Intent pilinIntent = new Intent(getApplicationContext(),PilinProfile.class);
+                        startActivity(pilinIntent);
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_Mohammed:
+                        Intent mohamedIntent = new Intent(getApplicationContext(),BenProfile.class);
+                        startActivity(mohamedIntent);
+                        drawerLayout.closeDrawers();
+
+                        break;
+                    case R.id.nav_Solomon:
+                        Intent solomanIntent = new Intent(getApplicationContext(),SolomanActivity.class);
+                        startActivity(solomanIntent);
+                        drawerLayout.closeDrawers();
+
+                        break;
+                    case R.id.nav_Robert:
+                        Intent robertIntent = new Intent(getApplicationContext(),RobertProfile.class);
+                        startActivity(robertIntent);
+                        drawerLayout.closeDrawers();
+
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
     }
 
     @Override
@@ -46,35 +80,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-
-        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        final NavigationView navigationView= findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.nav_Pilin:
-                        Intent pilinIntent = new Intent(getApplicationContext(),PilinActivity.class);
-                        startActivity(pilinIntent);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.nav_Mohammed:
-                        Intent mohamedIntent = new Intent(getApplicationContext(),MohamedActivity.class);
-                        startActivity(mohamedIntent);
-                        break;
-                    case R.id.nav_Solomon:
-                        Intent solomanIntent = new Intent(getApplicationContext(),SolomanActivity.class);
-                        startActivity(solomanIntent);
-                        break;
-                    case R.id.nav_Robert:
-                        Intent robertIntent = new Intent(getApplicationContext(),RobertActivity.class);
-                        startActivity(robertIntent);
-                        break;
-
-                }
-                return true;
-            }
-        });
 
 
 
