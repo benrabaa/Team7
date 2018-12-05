3131700953993005529684e165e40d07072b3c7a
rzmorales <robertzaratemorales@pursuit.org>
2018-12-04 22:05:45 -0500

    add navigation

--- app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/MainActivity.java
+++ app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/MainActivity.java
@@ -1,7 +1,12 @@
 package org.pursuit.group_portfolio_hw_team_se7en;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        final NavigationView navigationView= findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_Pilin:
+                        Intent pilinIntent = new Intent(getApplicationContext(),PilinActivity.class);
+                        startActivity(pilinIntent);
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_Mohammed:
+                        Intent mohamedIntent = new Intent(getApplicationContext(),MohamedActivity.class);
+                        startActivity(mohamedIntent);
+                        break;
+                    case R.id.nav_Solomon:
+                        Intent solomanIntent = new Intent(getApplicationContext(),SolomanActivity.class);
+                        startActivity(solomanIntent);
+                        break;
+                    case R.id.nav_Robert:
+                        Intent robertIntent = new Intent(getApplicationContext(),RobertActivity.class);
+                        startActivity(robertIntent);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
     }
 }
