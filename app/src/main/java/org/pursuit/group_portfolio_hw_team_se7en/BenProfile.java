c4c6464d8a9333b1fc1c2e819e1b45e2ea9bfaa8
rzmorales <robertzaratemorales@pursuit.org>
2018-12-09 17:10:18 -0500

    add drawable

--- app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/BenProfile.java
+++ app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/BenProfile.java
@@ -1,11 +1,15 @@
 package org.pursuit.group_portfolio_hw_team_se7en;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class BenProfile extends AppCompatActivity {
 
@@ -16,15 +20,44 @@ public class BenProfile extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_ben);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setType("text/email");
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"mohamedrabaa@pursuit.org"});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Hello! I found your profile.");
+                email.putExtra(Intent.EXTRA_TEXT, "Dear Mohamed," + "");
+                startActivity(Intent.createChooser(email, "Send Feedback:"));
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent browserIntent;
+        switch (item.getItemId()) {
+            case R.id.project1_ben:
+                Toast.makeText(this, "@string/project1_ben", Toast.LENGTH_SHORT).show();
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/benrabaa/CTES"));
+                startActivity(browserIntent);
+                return true;
+            case R.id.project2_ben:
+                Toast.makeText(this, "@string/project2_ben", Toast.LENGTH_SHORT).show();
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/benrabaa/Bank-Teller"));
+                startActivity(browserIntent);
+                return true;
+            case R.id.project3_ben:
+                Toast.makeText(this, "@string/project3_ben", Toast.LENGTH_SHORT).show();
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/benrabaa/Mad-Libs"));
+                startActivity(browserIntent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 }
--- app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/MainActivity.java
+++ app/src/main/java/org/pursuit/group_portfolio_hw_team_se7en/MainActivity.java
@@ -7,23 +7,19 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 
 
-
 public class MainActivity extends AppCompatActivity {
     public DrawerLayout drawerLayout;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -32,46 +28,41 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_button);
 
-         drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         final NavigationView navigationView1 = findViewById(R.id.nav_view);
 
         navigationView1.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.nav_Pilin:
-                        Intent pilinIntent = new Intent(getApplicationContext(),PilinProfile.class);
+                        Intent pilinIntent = new Intent(getApplicationContext(), PilinProfile.class);
                         startActivity(pilinIntent);
                         drawerLayout.closeDrawers();
                         break;
-                    case R.id.nav_Mohammed:
-                        Intent mohamedIntent = new Intent(getApplicationContext(),BenProfile.class);
+
+                    case R.id.nav_Mohamed:
+                        Intent mohamedIntent = new Intent(getApplicationContext(), BenProfile.class);
                         startActivity(mohamedIntent);
                         drawerLayout.closeDrawers();
-
                         break;
+
                     case R.id.nav_Solomon:
-                        Intent solomanIntent = new Intent(getApplicationContext(),SolomanActivity.class);
-                        startActivity(solomanIntent);
+                        Intent solomonIntent = new Intent(getApplicationContext(), SolomanProfile.class);
+                        startActivity(solomonIntent);
                         drawerLayout.closeDrawers();
-
                         break;
+
                     case R.id.nav_Robert:
-                        Intent robertIntent = new Intent(getApplicationContext(),RobertProfile.class);
+                        Intent robertIntent = new Intent(getApplicationContext(), RobertProfile.class);
                         startActivity(robertIntent);
                         drawerLayout.closeDrawers();
-
-
-
                         break;
-
                 }
                 return true;
             }
         });
-
-
     }
 
     @Override
@@ -82,8 +73,5 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-
-
-
     }
 }
